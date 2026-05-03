02a905c8bbfdf5acb5211ba011e4e250e7a3d0ce
Suraj Sedai <surajsedai05@gmail.com>
2026-05-03 16:31:44 -0400

    kv outline

---
+++ src/kv_cache/contiguous_cache.py
@@ -0,0 +1,18 @@
+class ContiguousKVCache:
+    def __init__(self, config, batch_size, max_seq_len, device, dtrype):
+        pass
+
+    def append(self, layer_ids, k, v):
+        pass
+    
+    def get_layer_cache(self, layer_idx):
+        pass
+
+    def reset(self):
+        pass
+
+    @property
+    def current_seq_len(self):
+        pass
+
+    
